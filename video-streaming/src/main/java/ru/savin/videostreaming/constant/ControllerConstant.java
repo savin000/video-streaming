12fc279e1ba906a228f28ba1fba52b9aabc0c8b4
Mikhail Savin <58307835+savin000@users.noreply.github.com>
2024-11-06 21:19:10 +0300

    Delete video-streaming directory

--- video-streaming/src/main/java/ru/savin/videostreaming/constant/ControllerConstant.java
+++
@@ -1,5 +0,0 @@
-package ru.savin.videostreaming.constant;
-
-public class ControllerConstant {
-    public static final String ROOT_PATH = "/api/video-streaming-service/";
-}
--- video-streaming/src/main/java/ru/savin/videostreaming/constant/GlobalExceptionHandlerMessageConstant.java
+++
@@ -1,6 +0,0 @@
-package ru.savin.videostreaming.constant;
-
-public class GlobalExceptionHandlerMessageConstant {
-    public static final String BASE_MESSAGE = "Unexpected exception occurred. ";
-    public static final String ENTITY_NOT_FOUND_MESSAGE = "Failed to find entity. ";
-}
